f3a01970b90b37ff7e8f3f6b42bc1765016f9ba4
Shin-NiL <niltonr@gmail.com>
2020-06-29 20:06:47 -0300

    Added package_build script

---
+++ tools/package_build.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+RELEASES_DIR="releases"
+VERSION_DIR="$RELEASES_DIR/$1"
+PLUGIN_DIR="$RELEASES_DIR/$1/admob-plugin"
+
+if [[ ! -e $RELEASES_DIR ]]; then
+    mkdir -p $RELEASES_DIR
+fi
+
+mkdir -p $PLUGIN_DIR
+cp -r ../admob-lib $VERSION_DIR
+cp ../config/GodotAdmob.gdap ../admob-plugin/godotadmob/build/outputs/aar/GodotAdmob.release.aar $PLUGIN_DIR 
+
+cd $VERSION_DIR
+zip -r "../GodotAdmobPlugin-${1}.zip" .
